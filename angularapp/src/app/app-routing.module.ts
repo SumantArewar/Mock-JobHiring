b38442fd2aefb984343c784de4f2d919444d7c7c
Iamneo <techadmin@iamneo.ai>
2023-11-26 15:48:02 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewJobPostingsComponent } from './view-job-postings/view-job-postings.component';
+import { ViewJobApplicationsComponent } from './view-job-applications/view-job-applications.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'view-post' , component : ViewJobPostingsComponent},
+  {path : 'view-appli' , component : ViewJobApplicationsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/view-job-postings/view-job-postings.component.ts
+++ angularapp/src/app/view-job-postings/view-job-postings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JobService } from '../services/job.service';
 
 @Component({
   selector: 'app-view-job-postings',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewJobPostingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private js : JobService) { }
 
   ngOnInit(): void {
   }
